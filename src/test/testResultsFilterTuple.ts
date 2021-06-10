ca44f33d91a1f9a9e2d9c72ae38de30a7eb6bd57
Lou Cyx <git@lou.cx>
2021-06-10 01:21:02 -0700

    :arrow_up: dependency upgrade.

--- src/test/testResultsFilterTuple.ts
+++ src/test/testResultsFilterTuple.ts
@@ -5,5 +5,6 @@ import { isPassedTestResult } from "./isPassedTestResult";
 /**
  * Filter tests results in a tuple `[passed, failed]`.
  */
-export const testResultsFilterTuple =
-	arrayFilterTuple<TestResult, TestResult>(isPassedTestResult);
+export const testResultsFilterTuple = arrayFilterTuple<TestResult, TestResult>(
+	isPassedTestResult
+);
