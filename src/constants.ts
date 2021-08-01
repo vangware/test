249aebe78383cd2e09ec8bfa327f16065ce3df16
Luke Shiru <hi@lshi.ru>
2021-07-31 18:51:23 -0700

    :arrow_up: dependency upgrade. :heavy_minus_sign: remove `husky` :tada: :heavy_plus_sign: add `@arkweid/lefthook`. :building_construction: change markdown files to use references. :bento: update logo in `README.md` :building_construction: update outDir to single `dist` directory.

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,17 @@
 import {
 	backgroundGreen,
 	backgroundRed,
+	foregroundBlack,
 	foregroundRed,
-	foregroundWhite,
 	mix
 } from "@vangware/ansi";
 import { comment } from "./utils/comment";
 
 /** Fail message with colors. */
-export const FAIL = mix([foregroundWhite, backgroundRed])("[FAIL]");
+export const FAIL = mix([foregroundBlack, backgroundRed])("[FAIL]");
 
 /** Pass message with colors. */
-export const PASS = mix([foregroundWhite, backgroundGreen])("[PASS]");
+export const PASS = mix([foregroundBlack, backgroundGreen])("[PASS]");
 
 /** Unwanted comment (`// Unwanted` in red text). */
 export const UNWANTED_COMMENT = comment(foregroundRed)("Unwanted");
