1f74cdd69ab96f38f3b55607bcdadb2af6f253ee
Lou Cyx <git@lou.cx>
2022-01-04 16:31:42 -0800

    :arrow_up: dependency upgrade. :bug: possible fix for Windows.

--- src/suite/suiteImport.ts
+++ src/suite/suiteImport.ts
@@ -1,4 +1,3 @@
-import { fileURLToPath } from "node:url";
 import type { SuiteImport } from "../types/SuiteImport.js";
 import type { URLOrString } from "../types/URLOrString.js";
 
@@ -8,10 +7,6 @@ import type { URLOrString } from "../types/URLOrString.js";
  * @category Suite
  */
 export const suiteImport = (path: URLOrString) =>
-	(
-		import(
-			typeof path === "string" ? path : fileURLToPath(path)
-		) as SuiteImport
-	).then(({ default: suite }) =>
+	(import(path as string) as SuiteImport).then(({ default: suite }) =>
 		typeof suite === "function" ? suite(path) : suite,
 	);
