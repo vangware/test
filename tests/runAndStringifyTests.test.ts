6ff327387cce62aee6e5c2405ea21174a76e162e
Luke Shiru <hi@luke.sh>
2022-08-09 16:12:23 -0700

    :building_construction: change runAndStringifyTests to sort results (fails at the bottom).

--- tests/runAndStringifyTests.test.ts
+++ tests/runAndStringifyTests.test.ts
@@ -3,14 +3,15 @@ import { FAIL, PASS, TEST } from "../src/constants.js";
 import { runAndStringifyTests } from "../src/runAndStringifyTests.js";
 import type { Tests } from "../src/types/Tests.js";
 
-const filename = "file:///example/test.js";
+const filenameA = "file:///example/test-a.js";
+const filenameB = "file:///example/test-b.js";
 
 export default [
 	{
 		given: "a passing test",
 		must: "return correct string",
 		received: runAndStringifyTests({
-			[filename]: [
+			[filenameA]: [
 				{
 					given: "🟢",
 					must: "🟩",
@@ -20,14 +21,14 @@ export default [
 			],
 		}),
 		wanted: `${TEST} ${underlined(
-			filename,
+			filenameA,
 		)}\n${PASS} Given ${bold`🟢`}, must ${bold`🟩`}.`,
 	},
 	{
 		given: "a failing test",
 		must: "return correct string",
 		received: runAndStringifyTests({
-			[filename]: [
+			[filenameA]: [
 				{
 					given: "🟢",
 					must: "🟩",
@@ -37,8 +38,108 @@ export default [
 			],
 		}).catch((error: string) => error),
 		wanted: `${TEST} ${underlined(
-			filename,
+			filenameA,
 		)}\n${FAIL} Given ${bold`🟢`}, must ${bold`🟩`}, but...\n\t└ it has the wrong value. Wanted ${foregroundBrightRed`"🟩"`} but received ${foregroundBrightRed`"❌"`}.`,
+	},
+	{
+		given: "a failing and a passing test in different files",
+		must: "return correct string",
+		received: runAndStringifyTests({
+			[filenameA]: [
+				{
+					given: "🟢",
+					must: "🟩",
+					received: "❌",
+					wanted: "🟩",
+				},
+			],
+			[filenameB]: [
+				{
+					given: "🟢",
+					must: "🟩",
+					received: "🟩",
+					wanted: "🟩",
+				},
+			],
+		}).catch((error: string) => error),
+		wanted: `${TEST} ${underlined(
+			filenameB,
+		)}\n${PASS} Given ${bold`🟢`}, must ${bold`🟩`}.\n${TEST} ${underlined(
+			filenameA,
+		)}\n${FAIL} Given ${bold`🟢`}, must ${bold`🟩`}, but...\n\t└ it has the wrong value. Wanted ${foregroundBrightRed`"🟩"`} but received ${foregroundBrightRed`"❌"`}.`,
+	},
+	{
+		given: "a failing and a passing test in the same file",
+		must: "return correct string",
+		received: runAndStringifyTests({
+			[filenameA]: [
+				{
+					given: "🟢",
+					must: "🟩",
+					received: "❌",
+					wanted: "🟩",
+				},
+				{
+					given: "🟢",
+					must: "🟩",
+					received: "🟩",
+					wanted: "🟩",
+				},
+			],
+		}).catch((error: string) => error),
+		wanted: `${TEST} ${underlined(
+			filenameA,
+		)}\n${PASS} Given ${bold`🟢`}, must ${bold`🟩`}.\n${FAIL} Given ${bold`🟢`}, must ${bold`🟩`}, but...\n\t└ it has the wrong value. Wanted ${foregroundBrightRed`"🟩"`} but received ${foregroundBrightRed`"❌"`}.`,
+	},
+	{
+		given: "a passing and a failing test in different files",
+		must: "return correct string",
+		received: runAndStringifyTests({
+			[filenameA]: [
+				{
+					given: "🟢",
+					must: "🟩",
+					received: "🟩",
+					wanted: "🟩",
+				},
+			],
+			[filenameB]: [
+				{
+					given: "🟢",
+					must: "🟩",
+					received: "❌",
+					wanted: "🟩",
+				},
+			],
+		}).catch((error: string) => error),
+		wanted: `${TEST} ${underlined(
+			filenameA,
+		)}\n${PASS} Given ${bold`🟢`}, must ${bold`🟩`}.\n${TEST} ${underlined(
+			filenameB,
+		)}\n${FAIL} Given ${bold`🟢`}, must ${bold`🟩`}, but...\n\t└ it has the wrong value. Wanted ${foregroundBrightRed`"🟩"`} but received ${foregroundBrightRed`"❌"`}.`,
+	},
+	{
+		given: "a passing and a failing test in the same file",
+		must: "return correct string",
+		received: runAndStringifyTests({
+			[filenameA]: [
+				{
+					given: "🟢",
+					must: "🟩",
+					received: "🟩",
+					wanted: "🟩",
+				},
+				{
+					given: "🟢",
+					must: "🟩",
+					received: "❌",
+					wanted: "🟩",
+				},
+			],
+		}).catch((error: string) => error),
+		wanted: `${TEST} ${underlined(
+			filenameA,
+		)}\n${PASS} Given ${bold`🟢`}, must ${bold`🟩`}.\n${FAIL} Given ${bold`🟢`}, must ${bold`🟩`}, but...\n\t└ it has the wrong value. Wanted ${foregroundBrightRed`"🟩"`} but received ${foregroundBrightRed`"❌"`}.`,
 	},
 	{
 		given: "an empty set of tests",
