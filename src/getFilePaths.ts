f8bb9f2f5d4f6be3af75377ed7e00ce7573a62a7
Luke Shiru <hi@luke.sh>
2022-08-09 15:43:34 -0700

    :building_construction: change getFilePaths to add support to run only one test.

--- src/getFilePaths.ts
+++ src/getFilePaths.ts
@@ -1,29 +1,45 @@
-import { readdir } from "node:fs/promises";
+import { readdir, stat } from "node:fs/promises";
 import { relativePath } from "./relativePath.js";
 import type { ReadOnlyURL } from "./types/ReadOnlyURL.js";
 import type { ReadOnlyURLs } from "./types/ReadOnlyURLs.js";
 
 /**
  * Recursively search for files in the given directory and filters them based on
- * given filterer function.
+ * given filterer function. If a file path is passed, it returns an array
+ * containing only that path.
  *
  * @category File System
  * @example
  * ```typescript
  * getFiles("./tests/"); // ["./tests/vangware.test.ts"]
  * ```
- * @param directory Directory to get files from.
+ * @param fileOrDirectory Directory to get files from.
  * @returns Array of files in given `directory` that match `filterer`.
  */
-export const getFilePaths = (directory: ReadOnlyURL): Promise<ReadOnlyURLs> =>
-	readdir(directory, { withFileTypes: true })
-		.then(direntArray =>
-			Promise.all(
-				direntArray.map(dirent =>
-					dirent.isDirectory()
-						? getFilePaths(new URL(`${dirent.name}/`, directory))
-						: new URL(dirent.name, directory),
-				),
-			).then(paths => paths.flat()),
-		)
-		.catch(() => Promise.reject(`Can't read ${relativePath(directory)}`));
+export const getFilePaths = (
+	fileOrDirectory: ReadOnlyURL,
+): Promise<ReadOnlyURLs> =>
+	stat(fileOrDirectory).then(stats =>
+		stats.isDirectory()
+			? readdir(fileOrDirectory, { withFileTypes: true })
+					.then(direntArray =>
+						Promise.all(
+							direntArray.map(dirent =>
+								dirent.isDirectory()
+									? getFilePaths(
+											new URL(
+												`${dirent.name}/`,
+												fileOrDirectory,
+											),
+									  )
+									: new URL(dirent.name, fileOrDirectory),
+							),
+						).then(paths => paths.flat()),
+					)
+					.catch(() =>
+						Promise.reject(
+							`Can't read ${relativePath(fileOrDirectory)}`,
+						),
+					)
+			: Promise.resolve([fileOrDirectory]),
+	);
